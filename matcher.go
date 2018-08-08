c8219c1b06b941d3a49bfb7c9bb30067d369d5f8
Zack Patrick <zpatrick319@Hotmail.com>
2018-08-07 22:20:43 -0700

    Add MatchAny and MatchAll

--- matcher.go
+++ matcher.go
@@ -4,6 +4,44 @@ package rbac
 // whether or not the target matches some pre-defined pattern.
 type Matcher func(target string) (bool, error)
 
+// MatchAny will convert a slice of Matchers into a single Matcher
+// that returns true if and only if at least one of the specified matchers returns true.
+func MatchAny(matchers ...Matcher) Matcher {
+	return func(target string) (bool, error) {
+		for _, matcher := range matchers {
+			match, err := matcher(target)
+			if err != nil {
+				return false, err
+			}
+
+			if match {
+				return true, nil
+			}
+		}
+
+		return false, nil
+	}
+}
+
+// MatchAny will convert a slice of Matchers into a single Matcher
+// that returns true if and only if all of the specified matchers returns true.
+func MatchAll(matchers ...Matcher) Matcher {
+	return func(target string) (bool, error) {
+		for _, matcher := range matchers {
+			match, err := matcher(target)
+			if err != nil {
+				return false, err
+			}
+
+			if !match {
+				return false, nil
+			}
+		}
+
+		return true, nil
+	}
+}
+
 // Always is a Matcher that always returns true
 func Always(target string) (bool, error) {
 	return true, nil
